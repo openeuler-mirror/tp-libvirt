b81f4170bcc75bc61bd33eccc56387f22e4160b6
Chen Qun <kuhn.chenqun@huawei.com>
2020-06-04 15:19:26 +0800

    hotplug_serial: add err msg for the return of two_devid
    The return massages maybe:'Error: the bus virtio-serial0.0
    -driver virtserialport set property failed'
    
    Signed-off-by: Chen Qun <kuhn.chenqun@huawei.com>

--- libvirt/tests/src/hotplug_serial.py
+++ libvirt/tests/src/hotplug_serial.py
@@ -232,7 +232,9 @@ def run(test, params, env):
                 err_o2 = "Parsing chardev args failed"
                 err_o3 = "Property 'virtserialport.chardev' can't"
                 err_o4 = "duplicate property"
-                if (err_o1 not in dup_o) and (err_o2 not in dup_o) and (err_o3 not in dup_o) and (err_o4 not in dup_o):
+                err_o5 = "set property failed"
+                if (err_o1 not in dup_o) and (err_o2 not in dup_o) and (err_o3 not in dup_o) and\
+                   (err_o4 not in dup_o) and (err_o5 not in dup_o):
                     test.fail("Expect fail, but run successfully:\n%s" % ret)
             else:
                 if "already exists" not in dup_e:
